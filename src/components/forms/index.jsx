b81f0f81a2ccbc32df217c5590efa6c66f2fbda4
sgujuluva <sanghee04@gmail.com>
2022-08-21 20:18:34 +0200

    dropdown and input in form

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -3,12 +3,19 @@ import React from 'react'
 import "./forms.css"
 function Forms() {
   return (
-    <div className="search">
+   
        <form >
     <input type="text" placeholder='Enter a task' />
-    <input type="button" value="Add" />
+   <button type="submit">Add</button>
+   <div className="select">
+    <select name="todos" >
+        <option value="all">All</option>
+        <option value="completed">Completed</option>
+        <option value="uncompleted">Uncompleted</option>
+    </select>
+   </div>
        </form>
-        </div>
+      
   )
 }
 
