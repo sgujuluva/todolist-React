69692dea93ed441720c8bc43a56dbbda616233e2
sgujuluva <sanghee04@gmail.com>
2022-08-21 20:31:25 +0200

    styling forms

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 //styles
-import "./forms.css"
+import "./forms.css";
 function Forms() {
   return (
-   
-       <form >
-    <input type="text" placeholder='Enter a task' />
-   <button type="submit">Add</button>
-   <div className="select">
-    <select name="todos" >
-        <option value="all">All</option>
-        <option value="completed">Completed</option>
-        <option value="uncompleted">Uncompleted</option>
-    </select>
-   </div>
-       </form>
-      
-  )
+    <div className="form-style">
+    <form>
+      <input type="text" placeholder="Enter a task" />
+      <button type="submit">Add</button>
+      <div className="select">
+        <select name="todos">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+    </div>
+  );
 }
 
-export default Forms
+export default Forms;
